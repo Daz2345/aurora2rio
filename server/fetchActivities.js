4b454a5c5e3d04fcc8239d143050bc9ff685c00d
daz2345 <darrenimpey@btinternet.com>
2016-06-09 15:48:34 +0000

    change to fetching

--- server/fetchActivities.js
+++ server/fetchActivities.js
@@ -1,24 +1,28 @@
 function insertActivity(element, index, array) {
+    console.log('checking duplicate activity');
     if (!!Activities.findOne({id: element.id})) {
+        console.log('get activity username');
         var user = Meteor.users.findOne({id: element.athlete.id});
         if (!!user) {
             element.username = user.profile.fullName;
         } else {
             element.username = "Who Knows";
         }
-        
         Meteor.call('Activities.insert', element);
     }
 }
 
 Meteor.methods({
     'Activities.insert'(activity) {
+        console.log('insert activity');
         Activities.insert(activity);
     },
     'fetchActivities'() {
-    
+        console.log('fetching activities');
         Meteor.users.find().forEach(function(user) {
-
+            
+            console.log(user);
+            
             var options = {
                 "headers": {
                     "authorization": "Bearer " + user.services.strava.accessToken,
