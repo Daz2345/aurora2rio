bf466bf9ebd5f7a1be6ab45f915b690ff6a6b864
daz2345 <darrenimpey@btinternet.com>
2016-06-09 12:28:31 +0000

    initial commit

---
+++ server/main.js
@@ -0,0 +1,10 @@
+// Meteor.startup(function() {
+//     ServiceConfiguration.configurations.remove({
+//     service: "strava"
+//   });
+
+// ServiceConfiguration.configurations.upsert(
+//   { service: "strava" },
+//   { $set: { client_id: "11993", secret: "a058718c4f96474068ef98a77ac0ace146506d73" } }
+//  );
+// })
